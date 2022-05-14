4d35bc2bda52c2af6ac378aedc5da5c462de98c0
Braguini <wilson.braguini@capgemini.com>
2022-05-14 14:21:11 -0300

    Selenium ex 02/03

--- src/test/java/ExerciciosSelenium.java
+++ src/test/java/ExerciciosSelenium.java
@@ -15,6 +15,10 @@ public class ExerciciosSelenium {
         driver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[2]/div/textarea")).sendKeys("Rua Ana Pereira Mello, 253");
         driver.findElement(By.xpath("//*[@id=\"eid\"]/input")).sendKeys("braguinipy@gmail.com");
         driver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[4]/div/input")).sendKeys("11957248400");
+        driver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[5]/div/label[2]/input")).click();
+        driver.findElement(By.xpath("//*[@id=\"checkbox2\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"Skills\"]/option[35]")).click();
+        driver.findElement(By.xpath("//*[@id=\"country\"]/option[11]")).click();
 
     }
 }
